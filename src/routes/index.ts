a3200c02010cf5963b5d1e247b1ecb7a468ecd0b
Ilia Krylatykh <iliakrylatykh@gmail.com>
2025-07-05 15:28:36 +0200

    add products

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router, Request, Response } from 'express'
+import { products } from '../db/products'
 
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-	res.render('pages/index', {
-		title: 'Главная',
-		numberOfIterations: 42,
-	})
+	res.render('pages/index', { products })
 })
 
 export default router
